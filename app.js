46601c0c1b7666653502f7f2136a8398998c44c4
tony scialo <arscialo@gmail.com>
2016-05-10 19:19:36 -0400

    fade to black

--- app.js
+++ app.js
@@ -1,6 +1,45 @@
+var smokeMaker;
+
 $(function(){
+  //makeSmoke();
+  addShapeHover();
+  addShapeClickEvent();
+
+});
+
+
+function addShapeHover(){
   $('#shape').hover(
        function(){ $('#forestImage').addClass('forestZoom') },
        function(){ $('#forestImage').removeClass('forestZoom') }
-)
-});
+  )
+}
+
+function addShapeClickEvent(){
+  $('#shape').click(function(){
+
+    stopSmoke();
+
+    var forestImage = $('#forestImage');
+
+    //Gets the scale for the transform
+    var element = document.querySelector('#forestImage');
+    var scaleX = element.getBoundingClientRect().width / element.offsetWidth;
+
+    forestImage.css({
+      '-webkit-transform' : 'scale(' + scaleX + ')',
+      '-moz-transform'    : 'scale(' + scaleX + ')',
+      '-ms-transform'     : 'scale(' + scaleX + ')',
+      '-o-transform'      : 'scale(' + scaleX + ')',
+      'transform'         : 'scale(' + scaleX + ')'
+    });
+
+    //fade the forest
+    forestImage.addClass('forestFade');
+
+    $('#eyeGif').addClass('static-gif-clicked');
+
+    $('#viewport').removeClass('show-smoke');
+
+  })
+}
